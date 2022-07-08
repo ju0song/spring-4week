e94aa3798944714b71937463ac5a221c8798d38d
ju0song <cuobalee@naver.com>
2022-07-08 20:00:47 +0900

    no message

---
+++ 4week/src/main/java/sparta/week4/ProductMypriceRequestDto.java
@@ -0,0 +1,12 @@
+package sparta.week4;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
