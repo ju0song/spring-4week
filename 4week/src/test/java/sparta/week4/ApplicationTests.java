dd1196372b2b86d772476c15a739ceb00b72ac2b
ju0song <cuobalee@naver.com>
2022-07-08 13:40:32 +0900

    initial commit

---
+++ 4week/src/test/java/sparta/week4/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.week4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
