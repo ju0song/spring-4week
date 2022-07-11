d92b8eec7f58b989485706aca73a9c81b6627685
ju0song <cuobalee@naver.com>
2022-07-11 13:21:11 +0900

    no message

--- 4week/src/main/java/sparta/week4/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package sparta.week4;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sparta.week4.Entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> { }
